4fbd341ff892b89c041e5753fb960e208247073e
panxin <panxin109@qq.com>
2016-03-27 14:39:06 +0800

    for a test

--- web/src/main/java/graduation/hnust/simplebook/web/ApplicationWebStarter.java
+++ web/src/main/java/graduation/hnust/simplebook/web/ApplicationWebStarter.java
@@ -4,15 +4,28 @@
 
 package graduation.hnust.simplebook.web;
 
+import graduation.hnust.simplebook.user.model.User;
+import io.terminus.common.model.Response;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Author  : panxin
  * Date    : 8:20 PM 3/26/16
  */
+@RestController
 @SpringBootApplication
 public class ApplicationWebStarter {
+
+    @RequestMapping("/hello")
+    public User hello() {
+        User user = new User();
+        user.setId(1L);
+        user.setUserName("hahssa");
+        return Response.ok(user).getResult();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationWebStarter.class, args);
