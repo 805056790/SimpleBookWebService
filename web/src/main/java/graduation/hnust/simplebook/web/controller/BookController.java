b5f06d4a289454ad2d8d2e1e7f122f465a8e96da
panxin <panxin109@qq.com>
2016-04-10 12:39:11 +0800

    add book controller

---
+++ web/src/main/java/graduation/hnust/simplebook/web/controller/BookController.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2016. 杭州端点网络科技有限公司.  All rights reserved.
+ */
+
+package graduation.hnust.simplebook.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author  : panxin109@gmail.com
+ * Date    : 12:38 PM 4/10/16
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/book")
+public class BookController {
+}
