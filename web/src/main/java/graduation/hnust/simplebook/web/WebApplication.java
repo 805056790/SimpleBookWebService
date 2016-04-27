3094dcb9ea5d76c58ecf6ad8b2bee206567b9b40
panxin <panxin109@qq.com>
2016-04-27 23:01:58 +0800

    fix

--- web/src/main/java/graduation/hnust/simplebook/web/WebApplication.java
+++ web/src/main/java/graduation/hnust/simplebook/web/WebApplication.java
@@ -13,7 +13,9 @@ import org.springframework.context.annotation.ComponentScan;
  * Date    : 8:20 PM 3/26/16
  */
 @SpringBootApplication
-@ComponentScan({"graduation.hnust.simplebook"})
+@ComponentScan({"graduation.hnust.simplebook.user",
+        "graduation.hnust.simplebook.message",
+        "graduation.hnust.simplebook.common"})
 public class WebApplication {
 
     /**
